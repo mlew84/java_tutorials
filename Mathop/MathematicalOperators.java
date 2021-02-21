4fde1d87ae146e6dc7325f81eb6186253f3c0e3d
code <code@MauriceLewiss-MacBook-Pro.local>
2021-02-21 03:24:06 -0500

    Add num

---
+++ Mathop/MathematicalOperators.java
@@ -0,0 +1,12 @@
+public class MathematicalOperators {
+   public static void main(String[] args) {
+        //define variables
+        int x = 5;
+        int y = 3;
+        int z;
+
+        //simple addition
+        z = x + y;
+        System.out.println(z);
+    }
+}
