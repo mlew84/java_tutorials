ce8d1db6704f216324eb73e3ab4bb076ef3f9e7c
code <code@MauriceLewiss-MacBook-Pro.local>
2021-02-26 02:31:06 -0500

    castingdata

---
+++ CastingDatatypes/castingdatatypes.java
@@ -0,0 +1,12 @@
+package CastingDatatypes;
+
+public class castingdatatypes {
+    public static void main(String[] args) {
+        //default values
+        int i = 200;
+
+        //cast to a short
+        short s = (short)i;
+        System.out.println(s);
+    }
+}
