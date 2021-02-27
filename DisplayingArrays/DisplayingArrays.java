ad807bc3c47c4db63402a70ae95220affce6cd16
code <code@MauriceLewiss-MacBook-Pro.local>
2021-02-27 04:42:34 -0500

    DisplayArray

--- DisplayingArrays/DisplayingArrays.java
+++ DisplayingArrays/DisplayingArrays.java
@@ -2,10 +2,10 @@ package DisplayingArrays;
 
 public class DisplayingArrays {
     public static void main(String[] args) {
-        int[] intArray = new int[2];
+        int[] intArray = new int[3];
         intArray[0] = -5123;
         intArray[2] = 32;
 
-        System.out.println(intArray);
+        System.out.println(intArray[1]);
     }
 }
