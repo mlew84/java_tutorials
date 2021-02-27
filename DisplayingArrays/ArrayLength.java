ee59290c369291bd82794ebdb8c14622ca8c599d
code <code@MauriceLewiss-MacBook-Pro.local>
2021-02-27 04:38:08 -0500

    alength

---
+++ DisplayingArrays/ArrayLength.java
@@ -0,0 +1,8 @@
+package DisplayingArrays;
+
+public class ArrayLength {
+    public static void main(String[] args) {
+        int[] arr = {2,4,6,8,10,12,14,16,918,200};
+        System.out.println(arr.length);
+    }
+}
---
+++ DisplayingArrays/DisplayingArrays.java
@@ -0,0 +1,11 @@
+package DisplayingArrays;
+
+public class DisplayingArrays {
+    public static void main(String[] args) {
+        int[] intArray = new int[2];
+        intArray[0] = -5123;
+        intArray[2] = 32;
+
+        System.out.println(intArray);
+    }
+}
