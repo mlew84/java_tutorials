1be24b8bfe62a5c9abd1ab54640561c68fa3de1e
code <code@MauriceLewiss-MacBook-Pro.local>
2021-03-14 13:46:58 -0400

    conditionblocks

---
+++ ConditionalBlocks/ConditionalBlocks.java
@@ -0,0 +1,14 @@
+package ConditionalBlocks;
+
+public class ConditionalBlocks{
+
+    public static void main(String[] args) {
+        boolean b = true;
+
+        if(b){
+            System.out.println("inside the if-statement");
+        }
+
+            System.out.println("Outside of the if-statement");
+        }
+} 
