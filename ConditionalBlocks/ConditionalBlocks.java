4683117552a776c40101e66706f2a6941e691f48
code <code@MauriceLewiss-MacBook-Pro.local>
2021-03-14 15:13:22 -0400

    updateCblocks

--- ConditionalBlocks/ConditionalBlocks.java
+++ ConditionalBlocks/ConditionalBlocks.java
@@ -3,12 +3,21 @@ package ConditionalBlocks;
 public class ConditionalBlocks{
 
     public static void main(String[] args) {
-        boolean b = true;
+        boolean firstCondition = false;
+        boolean secondCondition = true;
 
-        if(b){
+        if(firstCondition){
             System.out.println("inside the if-statement");
+
+        if (secondCondition){
+            System.out.println("inside the nested if-statement");
+        }
+        }else if(secondCondition){
+            System.out.println(5);
+        }else{
+            System.out.println("inside the else-statement");
         }
 
-            System.out.println("Outside of the if-statement");
+            System.out.println("outside the if-statement");
         }
 } 
